badbe96be44a7606122b9867097a804aee69d790
Vishwajit <vishwajitsarakpatil@email.com>
2025-07-02 11:41:01 +0530

    chatbot_with_tool

--- src/langgraphagenticai/ui/streamlitui/display_result.py
+++ src/langgraphagenticai/ui/streamlitui/display_result.py
@@ -22,3 +22,25 @@ class DisplayResultStreamlit:
                         st.write(user_message)
                     with st.chat_message("assistant"):
                         st.write(value["messages"].content)
+
+        elif usecase == "Chatbot With Web":
+            ## prepare the state and invoke the state graph
+            initial_state = {"messages":[user_message]}
+            res = graph.invoke(initial_state)
+            for message in res['messages']:
+                if type(message)  == HumanMessage:
+                    with st.chat_message("user"):
+                        st.write(message.content)
+                elif type(message) == ToolMessage:
+                    with st.chat_message("ai"):
+                        st.write("Tool call start")
+                        st.write(message.content)
+                        st.write("Tool Call End")
+
+                elif type(message)==AIMessage and message.content:
+                    with st.chat_message("assistant"):
+                        st.write(message.content)        
+
+
+
+
--- src/langgraphagenticai/ui/uiconfigfile.py
+++ src/langgraphagenticai/ui/uiconfigfile.py
@@ -6,14 +6,13 @@ class Config:
         self.config.read(config_file)
 
     def get_llm_options(self):
-        return self.config["DEFAULT"].get("LLM_OPTIONS").split(", ")
+        return [item.strip() for item in self.config["DEFAULT"].get("LLM_OPTIONS").split(",")]
 
     def get_usecase_options(self):
-        return self.config["DEFAULT"].get("USECASE_OPTIONS").split(", ")
+        return [item.strip() for item in self.config["DEFAULT"].get("USECASE_OPTIONS").split(",")]
 
     def get_groq_model_options(self):
-        return self.config["DEFAULT"].get("GROQ_MODEL_OPTIONS").split(", ")
+        return [item.strip() for item in self.config["DEFAULT"].get("GROQ_MODEL_OPTIONS").split(",")]
 
     def get_page_title(self):
         return self.config["DEFAULT"].get("PAGE_TITLE")
-
